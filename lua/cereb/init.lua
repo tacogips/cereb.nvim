b32b393dbdc7e10909950bd8f24d17108a3c0a75
tacogips <me@tacogips.me>
2024-10-07 11:32:07 +0900

    query with new input

--- lua/cereb/init.lua
+++ lua/cereb/init.lua
@@ -18,6 +18,20 @@ local function cereb_page()
 	cereb_cli.query_and_append_to_buffer_just_response(page_contents, config.cereb_bin_path)
 end
 
+local function cereb_page_with_new_input()
+	local lines = api.nvim_buf_get_lines(0, 0, -1, false)
+
+	local user_input = vim.fn.input("your new query: ")
+	if user_input == nil or user_input == "" then
+		vim.notify("canceled")
+		return
+	end
+	local page_contents = table.concat(lines, "\n")
+	page_contents = page_contents .. "\n" .. "cereb-user\n" .. "---\n" .. user_input
+
+	cereb_cli.query_and_append_to_buffer_with_latest_query(page_contents, config.cereb_bin_path)
+end
+
 local function cereb_selected()
 	local selected_text = buf.get_visual_selection()
 	if selected_text == nil then
@@ -36,6 +50,12 @@ function M.setup(user_options)
 	config = vim.tbl_deep_extend("force", config, user_options)
 
 	api.nvim_create_user_command("CerebPageMd", cereb_page, { nargs = 0, desc = "query current page as markdown" })
+	api.nvim_create_user_command(
+		"CerebPageMdWithQuery",
+		cereb_page_with_new_input,
+		{ nargs = 0, desc = "query current page as markdown with new input" }
+	)
+
 	api.nvim_create_user_command(
 		"CerebCurrentLineMd",
 		cereb_current_line,
